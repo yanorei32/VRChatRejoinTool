72b9f37e7289f18a292383a5e09ace60960cb1ef
KisaragiEffective <kisaragi.effective@gmail.com>
2021-09-02 18:39:43 +0900

    rename local vars

--- src/VRChat.cs
+++ src/VRChat.cs
@@ -6,8 +6,8 @@ using System.Windows.Forms;
 
 static class VRChat {
 	public static void SaveInstanceToShortcut(Instance i, string filepath, bool httpLink = false) {
-		var t = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8"));
-		dynamic shell = Activator.CreateInstance(t);
+		var dynShellType = Type.GetTypeFromCLSID(new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8"));
+		dynamic shell = Activator.CreateInstance(dynShellType);
 		var shortcut = shell.CreateShortcut(filepath);
 
 		shortcut.IconLocation = Application.ExecutablePath + ",0";
