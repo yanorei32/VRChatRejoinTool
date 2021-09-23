dbecef8095ec94f39c25dfbf401d11d82896f02b
yanorei32 <yanorei@hotmail.co.jp>
2021-09-24 01:34:54 +0900

    Fix worldid regex

--- src/Instance.cs
+++ src/Instance.cs
@@ -7,7 +7,7 @@ class Instance {
 	static Regex instanceNameR	= new Regex(@"\A[A-Za-z0-9]+\z");
 	static Regex nonceR			= new Regex(@"\A([0-9A-F]{48}|[0-9A-F]{64}|[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12})\z");
 	static Regex userIdR		= new Regex(@"\Ausr_[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}\z");
-	static Regex worldIdR		= new Regex(@"\Awr?ld_[a-f0-9]{8}-[a-f0-9]{4}-4[a-f0-9]{3}-[a-f0-9]{4}-[a-f0-9]{12}\z");
+	static Regex worldIdR		= new Regex(@"\Awr?ld_[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}\z");
 	static Regex regionR		= new Regex(@"\A[a-z]{1,3}\z");
 
 	public InstanceArgument[] ArgumentOrder { get; set; }
